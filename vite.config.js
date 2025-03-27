68bc157a44e8e06a04f656bdfc3dbce48f156d70
2701kai <2701kai@gmail.com>
2025-03-27 15:20:16 +0100

    restored accidently deleted lines of code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import UnoCSS from "unocss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react(), UnoCSS()],
+// });
+import React from "@vitejs/plugin-react";
+import UnoCSS from "unocss/vite";
+
+export default {
+  plugins: [UnoCSS(), React()],
+};
